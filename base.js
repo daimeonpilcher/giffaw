c5df95924db1e2c002ba1bd9fb8e179820ac348e
DAIMEON PILCHER <daimeon.pilcher@gmail.com>
2015-05-11 22:17:31 -0700

    displayed mockData

--- base.js
+++ base.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
   // code in here!
-  console.log("here's your data:", mockData)
+    // alert("loaded");
+  render(mockData);
 })
+
+function render(giphy){
+  var data = giphy.data;
+  data.forEach(function(item,i){
+    var thumb = item.images.fixed_height.url;
+    $("body").append("<img src='" + thumb + "'>"); // mind the single vs. double quotes!
+  })  
+}
